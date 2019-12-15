3a96cde256e2a9a3dc6c7dcf83b0dbe863a4d520
mercatel <mercatel86@gmail.com>
2019-12-15 11:16:19 +0500

    work javascript widjet date_input

--- mk/urls.py
+++ mk/urls.py
@@ -10,7 +10,7 @@ from mk.views import ChecklistView, MkReport, MkReportEtc, mk_nkro_report
 urlpatterns = [
     path('check_list_mk/', views.form_check_list_mk, name='check_list_mk_url'),
     path('mk_report/', MkReport.as_view(), name='mk_report_url'),
-    path('mk_etc_report/', MkReportEtc.as_view(), name='mk_etc_report_url'),
+    # path('mk_etc_report/', MkReportEtc.as_view(), name='mk_etc_report_url'),
     path('mk_nkro_report/', mk_nkro_report, name='mk_nkro_report_url'),
     path('quality_report/<str:slug>/', ChecklistView.as_view(), name='view_checklist_url'),
     path('search_shop/', views.search_shop, name='mk_search_shop_url'),
